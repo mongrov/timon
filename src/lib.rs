6dfc2188dcd65d64c5228c1522e794851975f158
ahmed.boutaraa <ahmed@mongrov.com>
2024-06-10 05:36:45 +0100

    add write_parquet_file module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod read_parquet_file;
+mod write_parquet_file;
 pub use read_parquet_file::read_parquet_file;
+pub use write_parquet_file::write_json_to_parquet;
 
 /// cbindgen:ignore
 #[cfg(target_os = "android")]
 pub mod android {
-    use crate::read_parquet_file::read_parquet_file;
+    use crate::{read_parquet_file::read_parquet_file, write_parquet_file::write_json_to_parquet};
     use jni::JNIEnv;
     use jni::objects::{JClass, JString};
     use jni::sys::jstring;
@@ -30,6 +32,36 @@ pub mod android {
             },
             Err(_) => {
                 let error_message = env.new_string("Error reading Parquet file")
+                    .expect("Couldn't create java string!");
+
+                error_message.into_raw() // Use into_raw to return the jstring
+            },
+        }
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn Java_expo_modules_testrustmodule_TestRustModule_writeJsonToParquet(
+        mut env: JNIEnv,
+        _class: JClass,
+        file_path: JString,
+        json_data: JString,
+    ) -> jstring {
+        let rust_file_path: String = env.get_string(&file_path)
+            .expect("Couldn't get java string!")
+            .into();
+        let rust_json_data: String = env.get_string(&json_data)
+            .expect("Couldn't get java string!")
+            .into();
+
+        match write_json_to_parquet(&rust_file_path, &rust_json_data) {
+            Ok(_) => {
+                let success_message = env.new_string("Successfully wrote JSON data to Parquet file")
+                    .expect("Couldn't create java string!");
+
+                success_message.into_raw() // Use into_raw to return the jstring
+            },
+            Err(e) => {
+                let error_message = env.new_string(format!("Error writing JSON data to Parquet file: {:?}", e))
                     .expect("Couldn't create java string!");
 
                 error_message.into_raw() // Use into_raw to return the jstring
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod read_parquet_file;
+mod write_parquet_file;
 pub use read_parquet_file::read_parquet_file;
+pub use write_parquet_file::write_json_to_parquet;
 
 fn main() {
+  // **************** read_parquet_file **************** //
   let file_path = "/tmp/greptimedb/data/greptime/public/1024/1024_0000000000/9d5ef3a4-9499-4da5-87ac-00a666ae70e3.parquet";    
   match read_parquet_file(file_path) {
     Ok(result) => {
@@ -10,5 +13,21 @@ fn main() {
       }
     }
     Err(e) => eprintln!("Failed to read the Parquet file: {:?}", e),
+  }
+
+  // **************** write_json_to_parquet **************** //
+  let file_path = "/tmp/greptimedb/sodium/example.parquet";
+
+  let json_data = r#"
+  [
+      {"name": "Alice", "age": 30},
+      {"name": "Bob", "age": 25},
+      {"name": "Charlie", "age": 35}
+  ]
+  "#;
+
+  match write_json_to_parquet(file_path, json_data) {
+      Ok(_) => println!("Successfully wrote JSON data to Parquet file {:?}", json_data),
+      Err(e) => eprintln!("Failed to write JSON data to Parquet file: {:?}", e),
   }
 }
